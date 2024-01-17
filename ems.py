50f449aca655199c92bfe31254935bf1e13f53e8
alexys <alexisaglar@gmail.com>
2024-01-17 17:35:03 +0000

    created solar irradiance model based on Beta PDF

--- ems.py
+++ ems.py
@@ -1,38 +1,60 @@
-class EnergyManagementSystem:
-    def __init__(self):
-        # Initialize the battery charge level (in kWh)
-        self.battery_charge = 10  # Example: 10 kWh initial charge
-        self.battery_capacity = 20  # Example: 20 kWh capacity
+import scipy.special as sp
+import pandas as pd
+import numpy as np
 
-        # Example data for solar production and house load (in kWh)
-        self.solar_production = [5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1, 0]  # Hourly production
-        self.house_load = [2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 2, 2]  # Hourly consumption
+data_file = 'data/solar_historical_data.csv'
 
-    def simulate_one_hour(self, hour):
-        solar = self.solar_production[hour]
-        load = self.house_load[hour]
+def read_data(data_file):
+    #Clean data from irradiation ready to process
+    with open(data_file, newline='') as weather_data:
+        irradiance = pd.read_csv(weather_data, skiprows=42, delimiter=';')
+        irradiance.rename(columns={irradiance.columns[0]: irradiance.columns[0].lstrip('# ')}, inplace=True)
+        irradiance['index_date'] = irradiance['Observation period'].apply(lambda x: x.split('/')[0])
+        irradiance['index_date'] = pd.to_datetime(irradiance['index_date'])
+        irradiance.set_index(irradiance['index_date'], inplace=True)
+        irradiance = irradiance[['DHI', 'GHI']]
+    return irradiance 
 
-        net_production = solar - load  # Net solar production after household consumption
+def data_normalization(irradiance_data):
+    # Normalize the data from PD
+    data_min = irradiance_data['GHI'].min()
+    data_max = irradiance_data['GHI'].max()
+    normalized_data = (irradiance_data - data_min) / (data_max - data_min)
+    return normalized_data
 
-        # Charge or discharge the battery based on net production
-        if net_production > 0:
-            # Charge the battery
-            charge_potential = self.battery_capacity - self.battery_charge
-            charge_amount = min(net_production, charge_potential)
-            self.battery_charge += charge_amount
-        elif net_production < 0:
-            # Discharge the battery
-            discharge_amount = min(-net_production, self.battery_charge)
-            self.battery_charge -= discharge_amount
+def beta_alpha_stimation(data_mean, data_std):
+    for i, _ in enumerate(data_mean):
+        if data_std.iloc[i] > 0:
+            beta_values['b'].iloc[i] = (1 - data_mean.iloc[i]) * ((data_mean.iloc[i] * (1 + data_mean.iloc[i])) / (data_std.iloc[i]**2) - 1)
+            beta_values['a'].iloc[i] = (data_mean.iloc[i] * beta_values['b'].iloc[i]) / (1 - data_mean.iloc[i])
+        else:
+            beta_values['b'].iloc[i] = 0
+            beta_values['a'].iloc[i] = 0
+    return beta_values
 
-        print(f"Hour {hour}:")
-        print(f"  Solar Production: {solar} kWh, House Load: {load} kWh")
-        print(f"  Battery Charge: {self.battery_charge:.2f} kWh\n")
+def beta_PDF_function(beta_values, day, time):
+    position = ((day - 1) * 24) + time
+    s = np.random.rand()
+    value = (sp.gamma(beta_values['a'].iloc[position] + beta_values['b'].iloc[position]) / (sp.gamma(beta_values['a'].iloc[position]) * sp.gamma(beta_values['b'].iloc[position])) ) * s * (1-s)**(beta_values['b'].iloc[position] - 1)
+    return value
 
-    def run_simulation(self):
-        for hour in range(12):  # Simulate for 12 hours
-            self.simulate_one_hour(hour)
+# Read data and clean data
+historical_irradiance = read_data(data_file)
+beta_values = pd.DataFrame(np.nan, index=range(len(historical_irradiance)),columns=['a','b'])
 
-if __name__ == "__main__":
-    ems = EnergyManagementSystem()
-    ems.run_simulation()
+# Normalize data
+normalized_irradiance = data_normalization(historical_irradiance)
+
+# Estimate parameters for beta distribution
+data_mean = normalized_irradiance['GHI'].groupby(normalized_irradiance.index.strftime('%m-%d %H')).mean()
+data_std = normalized_irradiance['GHI'].groupby(normalized_irradiance.index.strftime('%m-%d %H')).std()
+
+beta_values = beta_alpha_stimation(data_mean, data_std)
+beta_values.to_csv('data/beta_values.csv')
+
+# Beta PDF equation calculation
+day = 15
+time = 10 # in 24 hours format
+irradiance_prediction = beta_PDF_function(beta_values, day, time)
+print(irradiance_prediction)
+
